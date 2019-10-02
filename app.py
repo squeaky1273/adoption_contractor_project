10d3d7438724e7817c04c2c2a31f9d3978817d36
squeaky1273 <pjriddell22@gmail.com>
2019-10-01 22:21:46 -0700

    added submit route

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ def adoptions_new():
     """Create a new adoption ad"""
     return render_template('adoptions_new.html')
 
+@app.route('/adoptions', methods=['POST'])
+def playlists_submit():
+    """Submit a new adoption ad."""
+    print(request.form.to_dict())
+    return redirect(url_for('adoptions_index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
