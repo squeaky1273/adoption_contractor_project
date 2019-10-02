db1e3f9e332bfffaf38c78e5f5fed15911cc179a
squeaky1273 <pjriddell22@gmail.com>
2019-10-02 12:09:09 -0700

    created test class to hold unit test

--- test.py
+++ test.py
@@ -3,3 +3,21 @@
 from unittest import TestCase, main as unittest_main, mock
 from bson.objectid import ObjectId
 from app import app
+
+class PlaylistsTests(TestCase):
+    """Flask tests."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+        
+        # Get the Flask test client
+        self.client = app.test_client()
+
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
+
+
+
+
+if __name__ == '__main__':
+    unittest_main()
