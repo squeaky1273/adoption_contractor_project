3d73528d64b1d86d9e3b186df58cabe45bacba12
squeaky1273 <pjriddell22@gmail.com>
2019-10-03 15:40:48 -0700

    changed naming

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
+
 adoption_ads = db.adoptions
 
 app = Flask(__name__)
