89894f486e7ae11adb04983d2725049cab92eedc
squeaky1273 <pjriddell22@gmail.com>
2019-10-03 21:31:11 -0700

    added toy poodle png

--- test.py
+++ test.py
@@ -4,6 +4,25 @@ from unittest import TestCase, main as unittest_main, mock
 from bson.objectid import ObjectId
 from app import app
 
+sample_playlist_id = ObjectId('')
+sample_playlist = {
+    'name': 'Mr. Chew',
+    'breed': 'poodle'
+    'description': 'He is a cute dog, but is a handful.',
+    'price': '150',
+    'img_url': [
+
+    ]
+}
+
+sample_form_data = {
+    'name': sample_adoption['name'],
+    'breed': sample_adoption['breed'],
+    'description': sample_adoption['description'],
+    'price': sample_adoption['price'],
+    'img_url': sample_adoption['img_url']
+}
+
 class PlaylistsTests(TestCase):
     """Flask tests."""
 
@@ -28,6 +47,14 @@ class PlaylistsTests(TestCase):
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'New Adoption Ad', result.data)
 
+@mock.patch('pymongo.collection.Collection.find_one')
+def test_show_adoption(self, mock_find):
+    """Test showing a single adoption ad."""
+    mock_find.return_value = sample_playlist
+
+    result = self.client.get(f'/adoptions/{sample_adoption_id}')
+    self.assertEqual(result.status, '200 OK')
+    self.assertIn(b'Mr. Chew', result.data)
 
 if __name__ == '__main__':
     unittest_main()
